15d53183eda52d942619d7f06c758f3d3715f932
Maria Colio <mcoliot01@educantabria.es>
2025-12-18 19:26:11 +0100

    Update 2025_12_15_225038_create_proyectos_table.php

--- Version01/api/database/migrations/2025_12_15_225038_create_proyectos_table.php
+++ Version01/api/database/migrations/2025_12_15_225038_create_proyectos_table.php
@@ -4,30 +4,39 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
-        {
-            Schema::create('proyectos', function (Blueprint $table) {
-                $table->id();
-                $table->string('nombre', 100);
-                $table->text('descripcion')->nullable();
-                $table->string('color_fondo', 20)->nullable();
-                $table->string('imagen_fondo')->nullable();
-                // más adelante podremos añadir: id_equipo, estado, etc.
-                $table->timestamps();
-            });
-        }
+    {
+        Schema::create('proyecto', function (Blueprint $table) {
+            $table->id(); // PK
 
+            $table->unsignedInteger('id_equipo'); // FK
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->date('fecha_creacion');
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin_prevista');
 
-    /**
-     * Reverse the migrations.
-     */
+            $table->unsignedInteger('id_estado_proyecto'); // FK
+
+            $table->timestamps();
+
+            // FKs
+            $table->foreign('id_equipo')
+                ->references('id_equipo')
+                ->on('equipo')
+                ->onDelete('cascade');
+
+            $table->foreign('id_estado_proyecto')
+                ->references('id_estado_proyecto')
+                ->on('estado_proyecto')
+                ->onDelete('restrict');
+        });
+    }
+
     public function down(): void
     {
-        Schema::dropIfExists('proyectos');
+        Schema::dropIfExists('proyecto');
     }
 };
+
