fa813cba2b2c9f36e7a9c419641f6956b5b3daa2
darioobg <dbriongosg02@educantabria.es>
2025-12-22 15:50:50 +0100

    controllers

--- Version01/api/routes/api.php
+++ Version01/api/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Controllers
-use App\Http\Controllers\Api\ProyectoController;
+use App\Http\Controllers\ProyectoController;
 use App\Http\Controllers\TareaController;
 use App\Http\Controllers\ComentarioController;
 use App\Http\Controllers\NotificacionController;
