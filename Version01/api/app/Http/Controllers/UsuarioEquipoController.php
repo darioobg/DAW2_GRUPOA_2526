91dd98de65a428930b8cf8a95e19d082c1ae7182
Jino Olivera <joliverar2401@educantabria.es>
2026-01-07 16:08:28 +0100

    Remove idEquipo parameter from update method

--- Version01/api/app/Http/Controllers/UsuarioEquipoController.php
+++ Version01/api/app/Http/Controllers/UsuarioEquipoController.php
@@ -36,7 +36,7 @@ class UsuarioEquipoController extends Controller
         }
     }
 
-    public function update(Request $request, int $idUsuario, int $idEquipo): JsonResponse
+    public function update(Request $request, int $idUsuario): JsonResponse
     {
         try {
             $registro = $this->service->actualizar($idUsuario, $idEquipo, $request->all());
